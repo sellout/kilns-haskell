6cd66e1bed6eb5ad76377925cd8480615cca2052
Greg Pfeil <greg@technomadic.org>
2013-11-20 23:40:12 -0700

    Have the interpreter actually reduce the process.

--- source/Language/Kilns/Interpreter/Main.hs
+++ source/Language/Kilns/Interpreter/Main.hs
@@ -1,7 +1,13 @@
+{-#
+  LANGUAGE
+  UnicodeSyntax
+  #-}
+
 module Main
     (main) where
 
 import Data.Set (Set)
+import qualified Data.Set as Set
 import System.Environment
 
 import Text.Derp
@@ -9,10 +15,18 @@ import Text.Derp
 import Language.KellCalculus.AST
 import Language.KellCalculus.FraKtal
 import Language.KellCalculus.Parser
+import Language.KellCalculus.ReductionSemantics
 
 parse :: String -> Set (Process FraKtal)
 parse s = runParse (process grammar)
           (map (\x -> Token x [x]) ("(par " ++ s ++ "\n)"))
 
+reduceFully :: Pattern ξ ⇒ Process ξ → Process ξ
+reduceFully p = case reduce p of
+                  Just p' -> reduceFully p'
+                  Nothing -> p
+ 
 main :: IO ()
-main = getArgs >>= readFile . (flip (!!) 0) >>= print . parse 
+main = getArgs
+       >>= readFile . (flip (!!) 0)
+       >>= print . Set.map reduceFully . parse
