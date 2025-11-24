a3a5e5e94983fcddebed0ba4279801e4a535fb16
Greg Pfeil <greg@technomadic.org>
2025-11-24 16:57:01 -0700

    Sync latest Flaky template

--- .config/project/default.nix
+++ .config/project/default.nix
@@ -1,10 +1,9 @@
+### All available options for this file are listed in
+### https://sellout.github.io/project-manager/options.xhtml
 {
   config,
-  flaky,
   lib,
-  pkgs,
   self,
-  supportedSystems,
   ...
 }: {
   project = {
@@ -25,28 +24,7 @@
 
   imports = [./hlint.nix];
 
-  ## dependency management
-  services.renovate.enable = true;
-
-  ## development
-  programs = {
-    direnv.enable = true;
-    # This should default by whether there is a .git file/dir (and whether it’s
-    # a file (worktree) or dir determines other things – like where hooks
-    # are installed.
-    git.enable = true;
-  };
-
-  ## formatting
-  editorconfig.enable = true;
-
-  programs = {
-    treefmt.enable = true;
-    vale.enable = true;
-  };
-
   ## CI
-  services.garnix.enable = true;
   ## FIXME: Shouldn’t need `mkForce` here (or to duplicate the base contexts).
   ##        Need to improve module merging.
   services.github.settings.branches.main.protection.required_status_checks.contexts =
@@ -75,7 +53,6 @@
   };
 
   ## publishing
-  services.github.enable = true;
   services.github.settings.repository.topics = [
     "distributed-computing"
     "process-calculi"
